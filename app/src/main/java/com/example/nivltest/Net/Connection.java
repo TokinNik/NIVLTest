4047803509bc57bcd0a3bd2d8b9e6de0f9fb4b0a
TokovojNR <TokinNik@yandex.ru>
2019-10-08 03:53:39 +0700

    First commit

---
+++ app/src/main/java/com/example/nivltest/Net/Connection.java
@@ -0,0 +1,4 @@
+package com.example.nivltest.Net;
+
+public class Connection {
+}
---
+++ app/src/main/java/com/example/nivltest/UI/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.nivltest.UI;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nivltest.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
